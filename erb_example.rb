8c5ef00c2367f9b2463a733f7bd7fb5b63bd9e96
Douglas Manzelmann <dougamanzelmann@gmail.com>
2013-04-17 16:05:09 -0700

    Add doug.html.erb

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,15 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+y = "The meaning of everything is #{x}"
+favorite_things_sfw = ['good beer', 'cigarettes', 'nsfw']
+beer = { favorite: 'dogfish head' }
+
+template = ERB.new "The value of x is: <%= x %>
+                    <%= y %>
+                    <% favorite_things_sfw.each do |thing| %>
+                      I love <%= thing %>
+                    <% end %>
+                    My favorite brewer is <%= beer[:favorite] %>"
+
 puts template.result(binding)
